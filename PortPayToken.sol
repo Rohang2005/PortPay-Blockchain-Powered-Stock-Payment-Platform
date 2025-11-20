1658ddad39679116ae01acf276a2d28ab506e4d3
Rohang2005 <157229021+Rohang2005@users.noreply.github.com>
2025-11-20 16:20:30 +0530

    Update PortPayToken.sol

--- PortPayToken.sol
+++ PortPayToken.sol
@@ -1,1 +1,15 @@
+pragma solidity ^0.8.20;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/access/Ownable.sol";
+
+contract PortPayToken is ERC20, Ownable {
+    constructor(string memory tokenName, string memory tokenSymbol)
+        ERC20(tokenName, tokenSymbol)
+        Ownable(msg.sender)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
